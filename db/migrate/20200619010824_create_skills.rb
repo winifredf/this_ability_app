c0b9537e302e667c508c435679d85b59c3566cd9
winifredf <winifred.dykes@hotmail.com>
2020-06-18 21:16:21 -0400

    renamed abilities to skills as the pluralized abilities vs unpluralized ability was giving an error whereas it is easier and less messy to switch from singular and plural using the word skill

--- db/migrate/20200619010824_create_skills.rb
+++ db/migrate/20200619010824_create_skills.rb
@@ -1,6 +1,6 @@
-class Abilities < ActiveRecord::Migration
+class CreateSkills < ActiveRecord::Migration
   def change
-    create_table :abilities do |t|
+    create_table :skills do |t|
       t.string :title
       t.text :body
       t.integer :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200619003831) do
+ActiveRecord::Schema.define(version: 20200619010824) do
 
-  create_table "abilities", force: :cascade do |t|
+  create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "user_id"
